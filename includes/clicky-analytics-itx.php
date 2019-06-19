e88ff4a7698523a0fb96d64eeb46d19c2e4c209f
O.J <Olanrewaju@users.noreply.github.com>
2019-06-19 15:32:58 +0400

    Add files via upload

---
+++ includes/clicky-analytics-itx.php
@@ -0,0 +1,13 @@
+<?php
+/** 
+ * @package WordPress
+ * @subpackage Beisance|iSance Custom Theme For Beisance|Intelix
+ */
+
+?>
+
+<a title="Google Analytics Alternative" href="http://clicky.com/100984297"><img alt="Google Analytics Alternative" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(100984297); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100984297ns.gif" /></p></noscript>
+
---
+++ includes/google-analytics-itx.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The Sidebar containing the primary and secondary widget areas.
+ * @package WordPress
+ * @subpackage Beisance|iSance Custom Theme For Beisance|Intelix
+ */
+
+?>
+
+<div id="google-analytics"><!-- start #google-analytics-->
+
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-33249853-2', 'auto');
+	ga('send', 'pageview');
+</script>
+
+</div><!-- end #google-analytics-->
