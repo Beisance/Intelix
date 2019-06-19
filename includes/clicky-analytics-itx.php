6aa6a2fb6a5421bf085e313384f91368334f046b
O.J <Olanrewaju@users.noreply.github.com>
2019-06-19 15:37:59 +0400

    Update clicky-analytics-itx.php

--- includes/clicky-analytics-itx.php
+++ includes/clicky-analytics-itx.php
@@ -6,8 +6,5 @@
 
 ?>
 
-<a title="Google Analytics Alternative" href="http://clicky.com/100984297"><img alt="Google Analytics Alternative" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-<script src="//static.getclicky.com/js" type="text/javascript"></script>
-<script type="text/javascript">try{ clicky.init(100984297); }catch(e){}</script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100984297ns.gif" /></p></noscript>
+<!-- Enter your own Clicky Analytics tracking code here -->
 
